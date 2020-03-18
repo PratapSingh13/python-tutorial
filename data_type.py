e2abf153ad911a0b7fead4117f76682f625c16be
yogendra.pratap <yogendra.pratap@opstree.com>
2020-03-18 15:30:44 +0530

    Python data-types file added

---
+++ data_type.py
@@ -0,0 +1,30 @@
+#Getting the Data Type
+
+myvar = "Hello World"
+print(type(myvar))   # str Data Type
+
+myvar = 20
+print(type(myvar))   # int Data Type
+
+myvar = 20.5
+print(type(myvar))   # float Data Type
+
+myvar = 1a
+print(type(myvar))   # complex Data Type
+
+myvar = ["Yogendra", "Pratap", "Singh"]
+print(type(myvar))   # list Data type
+
+myvar = ("Yogendra", "Pratap", "Singh")
+print(type(myvar))   # tuple Data Type
+
+myvar = range(5)
+print(type(myvar))   # range Data Type
+
+myvar = {"name" : "Yogendra", "age" : 23}
+print(type(myvar))   # dict Data Type
+
+myvar = {"Yogendra", "Pratap", "Singh"}
+print(type(myvar))   # set Data Type
+
+
